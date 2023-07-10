ad41022a09f8a2b4daa24e051a64ea93ee920545
Dobroslav Tokoš <dobroslav@tokos.sk>
2023-07-10 16:24:10 +0200

    Added tauri skeleton files

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+	import {invoke} from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMessage = '';
+
+	async function greet() {
+		greetMessage = await invoke('greet', {name});
+	}
+</script>
+
+<div>
+	<input placeholder="Enter a name..." bind:value="{name}">
+	<button on:click="{greet}">Greet</button>
+	<p>{greetMessage}</p>
+</div>
